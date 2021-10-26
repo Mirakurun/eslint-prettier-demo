45db8eb8cda7f823507627ef5f5a6f8e7e06cd3a
Kevin Chhay <25449974+Mirakurun@users.noreply.github.com>
2021-10-25 18:42:26 -0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
+const func = arg => {
+  console.log(arg);
+};
+
+function output(x) {
+  if (x) {
+    return x;
+  } else {
+    return 'no arg';
+  }
+}
+
+function promise(x) {
+  return new Promise(function(resolve, reject) {
+    if (x) {
+      resolve(x);
+    }
+    reject('something bad happened');
+  });
+}
+
 exports.handler = async (event) => {
-  let data = 1;
-  const data2 =              2
-  const data3 = {
+  // console.log(event);
+
+  let data;
+  data.a.b = 3;
+  console.log(data);
+
+  const doubleQuotes = "double";
+  console.log(doubleQuotes);
+
+  const unusedVar = 5;
+
+  const z = x;
+
+  const hello = 'hello';
+  const concat = h + 'world';
+  console.log(concat);
+
+  const missingSemiColon = 'a'
+  console.log(missingSemiColon);
+
+  const indent =              1;
+            console.log(indent);
+
+  const trailingComma = {
     a: 1,
     b: 2,
     c: 3
+  };
+
+  if(data){
+    console.log('hi');
   }
 
-  data = 2;
-  data.a.b = 3;
-  
-  console.log(data);
-        console.log(event);
+  log('x');
+  func('hi');
+  output(5);
+  const p = await promise();
+  console.log(p);
 };
 
-function hello(x) {
+function log(x) {
   console.log(x);
 }
